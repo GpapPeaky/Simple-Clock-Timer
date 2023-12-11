cbed79cb7325b9acb52923f5c1a854a78729e8bf
Gpap Peaky <150977755+GpapPeaky@users.noreply.github.com>
2023-12-11 18:11:36 +0200

    Minor Changes, Added while(1) loop in clock

--- clock.c
+++ clock.c
@@ -10,34 +10,35 @@ int h = 0;
 
 void my_clock(void){
 
-    s++;
+    while(1){
+        s++;
 
-    if(s == 60){
-        s = 0;
-        m++;
-    }else if(m == 60){
-        m = 0;
-        h++;
-    }else if (h == 24){
-        s = 0;
-        m = 0;
-        h = 0;
+        if(s == 60){
+            s = 0;
+            m++;
+        }else if(m == 60){
+            m = 0;
+            h++;
+        }else if (h == 24){
+            s = 0;
+            m = 0;
+            h = 0;
+        }
+
+        printf("%02d : %02d : %02d\n\n",h,m,s);
+
+        sleep(1); /* Wait one second */
+
+        system("cls"); /* Clear the terminal, and display the new clock */
+
     }
-
-    printf("%02d : %02d : %02d\n\n",h,m,s);
-
-    sleep(1); /* Wait one second */
-
-    system("cls"); /* Clear the terminal, and display the new clock */
 
     return;
 }
 
 int main(){
 
-    while(1){
-        my_clock();
-    }
+    my_clock();
 
     return 0;
 }
